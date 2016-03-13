81fd72647374b7d6af8a1011c3c3efc3521828fa
Andriy <gandriys@gmail.com>
2016-03-13 13:10:52 +0200

    gulp, cards examples
    ready to concat JSONs gulp, some card examples

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,27 @@
 // Gulp
 var gulp = require('gulp');
+
+// Dependencies
+var concat_json = require("gulp-concat-json");
+
+// Concat JSON
+gulp.task('jsonIP', function() {
+    return gulp.src("dev/cards/in_progress/*.json")
+        .pipe(concat_json("cardsInProgress.json"))
+        .pipe(gulp.dest("dist/cards/"));
+});
+
+gulp.task('jsonReady', function() {
+    return gulp.src("dev/cards/ready/*.json")
+        .pipe(concat_json("cardsReady.json"))
+        .pipe(gulp.dest("dist/cards/"));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('dev/cards/in_progress/*.json', ['jsonIP']);
+    gulp.watch('dev/cards/ready/*.json', ['jsonReady']);
+});
+
+// Default Task
+gulp.task('default', ['watch', 'jsonIP', 'jsonReady']);
